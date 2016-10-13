76d289e274ab8b68e8177369555aba9597d46f82
Michael Levy <mike.levy.work@gmail.com>
2016-10-13 13:05:14 -0600

    Clean up description of get_put test
    Per Keith's suggestion, re-worded the description that appears when you run
    
    $ ./get_put.py -h

--- tests/unit_tests/get_put/get_put.py
+++ tests/unit_tests/get_put/get_put.py
@@ -5,7 +5,10 @@ path.insert(0,'../../python_for_tests')
 
 import marbl_testing as mt
 
-mt.parse_args(desc='Set all configuration variables and parameters via put statements, use get to ensure they are correct. Also put different values to make sure nothing else changes.', HaveNamelist=False)
+mt.parse_args(desc='Set all configuration variables and parameters via put '
+                   'statements and use get statements to ensure they are '
+                   'correct. Also put different values to make sure nothing '
+                   'else changes.', HaveNamelist=False)
 
 mt.build_exe()
 
