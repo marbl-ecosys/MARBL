marbl-ecosys/MARBL

commit f4add10a53889f157b30e5333b58bc26fb3a1096

Remove support for stand-alone GPTL library
If user wants GPTL timing, must use the wrappers provided by CIME

tests/driver_src/Makefile
@@ -3,7 +3,6 @@
 extension = .F90
 
 COMP_NAME=NONE
-GPTL_DIR=NONE
 USEMPI=FALSE
 
 OBJ_ROOT = $(realpath ../obj)
@@ -28,18 +27,9 @@ EXECPPDEFS=
 INCLUDES=-I$(INC_LOC)
 LINKS=-L$(LIB_DIR) $(LFLAG)
 
-ifneq ($(GPTL_DIR),NONE)
-  LIBCPPDEFS+= -DMARBL_TIMING_OPT=GPTL
-  EXECPPDEFS+= -DMARBL_WITH_GPTL
-  INCLUDES+= -I$(GPTL_DIR)/include
-  LINKS+= -L$(GPTL_DIR)/lib -lgptl
-endif
 EXE = $(EXE_DIR)/marbl.exe
 ifeq ($(USEMPI),TRUE)
-  ifeq ($(GPTL_DIR),NONE)
-    LIBCPPDEFS+= -DMARBL_TIMING_OPT=MPI
-  endif
-  EXECPPDEFS+= -DMARBL_WITH_MPI
+  LIBCPPDEFS+= -DMARBL_TIMING_OPT=MPI
   MPISUF=-mpi
   override FC = mpif90
   EXE = $(EXE_DIR)/marbl-mpi.exe

src/Makefile
@@ -1,7 +1,6 @@
 .SUFFIXES: .F90 .o
 extension = .F90
 
-GPTL_DIR=NONE
 USEMPI=FALSE
 
 SRC_DIR=.
@@ -9,7 +8,6 @@ OBJ_DIR=../tests/obj
 LIB_DIR=../lib
 INC_DIR=../include
 FC = NONE
-GPTL_DIR = NONE
 INCLUDES=
 
 ifneq ($(INC_DIR),$(OBJ_DIR))
@@ -33,15 +31,8 @@ OBJS = $(addprefix $(OBJ_DIR)/,${MODULES:.F90=.o})
 
 CPPDEFS=-DECOSYS_NT=27 -DZOOPLANKTON_CNT=1 -DAUTOTROPH_CNT=3 -DGRAZER_PREY_CNT=3
 
-ifneq ($(GPTL_DIR),NONE)
-  CPPDEFS+= -DMARBL_TIMING_OPT=GPTL
-  INCLUDES+= -I$(GPTL_DIR)/include
-endif
-
 ifeq ($(USEMPI),TRUE)
-  ifeq ($(GPTL_DIR),NONE)
-    CPPDEFS+= -DMARBL_TIMING_OPT=MPI
-  endif
+  CPPDEFS+= -DMARBL_TIMING_OPT=MPI
   MPISUF=-mpi
   override FC = mpif90
 endif
@@ -67,7 +58,7 @@ intel:
 	$(MAKE) $(LIB_DIR)/libmarbl-intel$(MPISUF).a USE_DEPS=TRUE FC=ifort FCFLAGS="-O2 -free -module $(OBJ_DIR)/intel$(MPISUF) -cpp -nogen-interface -fp-model source" OBJ_DIR=$(OBJ_DIR)/intel$(MPISUF) INC_DIR=$(INC_DIR)/intel$(MPISUF)
 
 pgi:
-	$(MAKE) $(LIB_DIR)/libmarbl-pgi.a USE_DEPS=TRUE FC=pgf90 FCFLAGS="-O2 -Mfree -module $(OBJ_DIR)/pgi OBJ_DIR=$(OBJ_DIR)/pgi" INC_DIR=$(INC_DIR)/pgi
+	$(MAKE) $(LIB_DIR)/libmarbl-pgi$(MPISUF).a USE_DEPS=TRUE FC=pgf90 FCFLAGS="-O2 -Mfree -module $(OBJ_DIR)/pgi$(MPISUF) OBJ_DIR=$(OBJ_DIR)/pgi$(MPISUF)" INC_DIR=$(INC_DIR)/pgi$(MPISUF)
 
 nag:
 	$(MAKE) $(LIB_DIR)/libmarbl-nag$(MPISUF).a USE_DEPS=TRUE FC=nagfor FCFLAGS="-O2 -free -I $(OBJ_DIR)/nag$(MPISUF) -mdir $(OBJ_DIR)/nag$(MPISUF) -kind=byte" OBJ_DIR=$(OBJ_DIR)/nag$(MPISUF) INC_DIR=$(INC_DIR)/nag$(MPISUF)

src/marbl_timing_mod.F90
@@ -2,9 +2,7 @@ module marbl_timing_mod
 
   ! For mutually-exclusive options for timers:
   ! -DMARBL_TIMING_OPT=CIME    => use perf_mod
-#define CIME 3
-  ! -DMARBL_TIMING_OPT=GPTL    => use GPTL library (FIXME: remove this option!)
-#define GPTL 2
+#define CIME 2
   ! -DMARBL_TIMING_OPT=MPI     => use MPI_Wtime()
 #define MPI 1
   ! MARBL_TIMING_OPT undefined => use cpu_time()
@@ -19,10 +17,6 @@ module marbl_timing_mod
 
   implicit none
   private
-
-#if MARBL_TIMING_OPT == GPTL
-#include "gptl.inc"
-#endif
 
 #if MARBL_TIMING_OPT == MPI
 #include "mpif.h"
@@ -182,10 +176,6 @@ Contains
     integer omp_get_num_threads
 #endif
 
-#if MARBL_TIMING_OPT == GPTL
-    integer gptl_retval
-#endif
-
     ! Error checking
     if (id.gt.self%num_timers) then
       write(log_message, "(I0,A)") id, ' is not a valid timer ID'
@@ -206,8 +196,6 @@ Contains
 #endif
 #if MARBL_TIMING_OPT == CIME
       call t_startf(trim(self%individual_timers(id)%name))
-#elif MARBL_TIMING_OPT == GPTL
-      gptl_retval = gptlstart(trim(self%individual_timers(id)%name))
 #endif
       timer%cur_start = get_time()
      end associate
@@ -225,10 +213,6 @@ Contains
     character(*), parameter :: subname = 'marbl_timing_mod:marbl_timing_stop'
     character(len=char_len) :: log_message
     real(r8) :: runtime
-
-#if MARBL_TIMING_OPT == GPTL
-    integer gptl_retval
-#endif
 
     ! Error checking
     if (id.gt.self%num_timers) then
@@ -248,8 +232,6 @@ Contains
       timer%is_running  = .false.
 #if MARBL_TIMING_OPT == CIME
       call t_stopf(trim(self%individual_timers(id)%name))
-#elif  MARBL_TIMING_OPT == GPTL
-      gptl_retval = gptlstop(trim(self%individual_timers(id)%name))
 #endif
       runtime = get_time() - timer%cur_start
       timer%cur_start = c0
@@ -277,19 +259,13 @@ Contains
 
   function get_time() result(cur_time)
 
-#if (MARBL_TIMING_OPT == CIME) || (MARBL_TIMING_OPT == GPTL)
+#if MARBL_TIMING_OPT == CIME
     real(r8) :: wall, usr, sys
-#endif
-#if MARBL_TIMING_OPT == GPTL
-    integer :: gptl_retval
 #endif
     real(r8) :: cur_time
 
 #if MARBL_TIMING_OPT == CIME
     call t_stampf(wall, usr, sys)
-    cur_time = wall
-#elif MARBL_TIMING_OPT == GPTL
-    gptl_retval = gptlstamp(wall, usr, sys)
     cur_time = wall
 #elif MARBL_TIMING_OPT == MPI
     cur_time = MPI_Wtime()

tests/python_for_tests/marbl_testing_class.py
@@ -14,7 +14,6 @@ class MARBL_testcase(object):
     self._compiler = None
     self._machine = None
     self._namelistfile = 'marbl_in'
-    self._GPTLroot = None
     self._mpitasks = 0
     self._marbl_dir = path.abspath('%s/../..' % path.dirname(__file__))
 
@@ -46,9 +45,6 @@ class MARBL_testcase(object):
 
     parser.add_argument('-m', '--mach', action='store', dest='mach',
            help='machine to build on', choices=machs.supported_machines)
-
-    parser.add_argument('--gptl_loc', action='store', dest='gptl',
-                        default=None, help='location of GPTL library')
 
     parser.add_argument('--mpitasks', action='store', dest='mpitasks',
                         default=0, help='Number of MPI tasks (default: 0 => no MPI)')
@@ -101,10 +97,6 @@ class MARBL_testcase(object):
     if HaveNamelist:
       self._namelistfile = args.namelistfile
 
-    if args.gptl is not None:
-      self._GPTLroot = args.gptl
-      print "Loading GPTL library from %s" % self._GPTLroot
-
     self._mpitasks = int(args.mpitasks)
     print '----'
     sys.stdout.flush()
@@ -130,8 +122,6 @@ class MARBL_testcase(object):
       machs.load_module(self._machine, loc_compiler)
 
     makecmd = 'make %s' % loc_compiler
-    if self._GPTLroot is not None:
-      makecmd += ' GPTL_DIR=%s' % self._GPTLroot
     if self._mpitasks > 0:
       makecmd += ' USEMPI=TRUE'
     sh_command('cd %s; %s' % (src_dir, makecmd))
@@ -150,8 +140,6 @@ class MARBL_testcase(object):
       machs.load_module(self._machine, loc_compiler)
 
     makecmd = 'make %s' % loc_compiler
-    if self._GPTLroot is not None:
-      makecmd += ' GPTL_DIR=%s' % self._GPTLroot
     if self._mpitasks > 0:
       makecmd += ' USEMPI=TRUE'
     sh_command('cd %s; %s' % (drv_dir, makecmd))

commit 173e34f0a0ec9b659c4e67830b888bc2394db1f8

Removing MARBL_WITH_MPI macro introduced bugs
f4add10 introduced unexpected bugs when I cleaned out GPTL ifdefs and tried to
streamline the Makefile, but MPI tests work again now.

tests/driver_src/Makefile
@@ -29,6 +29,7 @@ LINKS=-L$(LIB_DIR) $(LFLAG)
 
 EXE = $(EXE_DIR)/marbl.exe
 ifeq ($(USEMPI),TRUE)
+  EXECPPDEFS+= -DMARBL_TIMING_OPT=MPI
   LIBCPPDEFS+= -DMARBL_TIMING_OPT=MPI
   MPISUF=-mpi
   override FC = mpif90

tests/driver_src/marbl.F90
@@ -45,10 +45,6 @@ Program marbl
 
   Implicit None
 
-#ifdef MARBL_WITH_GPTL
-  include 'gptl.inc'
-#endif
-
   character(len=256), parameter :: subname = 'Program Marbl'
   type(marbl_interface_class) :: marbl_instance
   type(marbl_log_type)        :: driver_status_log
@@ -69,10 +65,6 @@ Program marbl
   ! (0) Initialization
   !     MPI?
   call marbl_mpi_init()
-  !     GPTL?
-#ifdef MARBL_WITH_GPTL
-  n = gptlinitialize()
-#endif
 
   !     Set up local variables
   !       * Empty strings used to pass namelist file contents to MARBL
@@ -264,20 +256,6 @@ Contains
 
     real(r8) :: min_runtime, ind_runtime, max_runtime, tot_runtime
     character(len=15) :: int_to_str
-
-#ifdef MARBL_WITH_GPTL
-    integer :: gptl_ret
-
-    if (lwrite_gptl_file) then
-#ifdef MARBL_WITH_MPI
-      gptl_ret = gptlpr_summary_file(MPI_COMM_WORLD, 'timing.txt')
-#else
-      gptl_ret = gptlpr(0)
-#endif
-    end if
-    gptl_ret = gptlfinalize()
-
-#endif
 
 100 format(A, ': ', F11.3, ' seconds',A)
 

tests/driver_src/marbl_mpi_mod.F90
@@ -1,12 +1,17 @@
 module marbl_mpi_mod
 
   ! This module contains wrappers for all MPI calls so that marbl.F90
-  ! does not need to have #if MARBL_WITH_MPI macros scattered throughout
+  ! does not need to have #ifdef macros scattered throughout
 
   Implicit None
   public
 
-#if MARBL_WITH_MPI
+#define MPI 1
+#if MARBL_TIMING_OPT == MPI
+#define MARBL_WITH_MPI
+#endif
+
+#ifdef MARBL_WITH_MPI
   include 'mpif.h'
   logical, parameter :: mpi_on = .true.
 #else
@@ -70,7 +75,7 @@ contains
     real(r8), intent(in) :: dbl_var
     integer,  intent(in)  :: receiver
 
-#if MARBL_WITH_MPI
+#ifdef MARBL_WITH_MPI
     integer :: ierr
 
     call MPI_Send(dbl_var, 1, MPI_DOUBLE_PRECISION, 0, 2017, MPI_COMM_WORLD, ierr)
@@ -87,7 +92,7 @@ contains
     real(r8), intent(out) :: dbl_var
     integer,  intent(in)  :: sender
 
-#if MARBL_WITH_MPI
+#ifdef MARBL_WITH_MPI
     integer :: status(MPI_STATUS_SIZE)
     integer :: ierr
 
@@ -108,7 +113,7 @@ contains
 
     integer :: ierr
 
-#if MARBL_WITH_MPI
+#ifdef MARBL_WITH_MPI
     call MPI_Bcast(str_to_bcast, len(str_to_bcast), MPI_CHARACTER, root_task, &
                    MPI_COMM_WORLD, ierr)
 #else
@@ -122,7 +127,7 @@ contains
 
   subroutine marbl_mpi_abort()
 
-#if MARBL_WITH_MPI
+#ifdef MARBL_WITH_MPI
     integer :: ierr
 
     call MPI_Abort(MPI_COMM_WORLD, ierr)