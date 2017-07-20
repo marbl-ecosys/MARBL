d457281a5d0bc4e3e11b6c40750407bf4f9df65b
kristenkrumhardt <kristen.krumhardt@colorado.edu>
2017-07-20 11:59:37 -0600

    Change parms back to the way they were with v2, except kNO3 for sp

--- src/marbl_parms.F90
+++ src/marbl_parms.F90
@@ -357,15 +357,15 @@ contains
         autotrophs(n)%kFe             = 0.03e-3_r8         ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kPO4            = 0.006_r8           ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kDOP            = 0.3_r8             ! in marbl_parms framework, see NOTE above
-        autotrophs(n)%kNO3            = 0.35_r8            ! in marbl_parms framework, see NOTE above
+        autotrophs(n)%kNO3            = 0.3_r8            ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kNH4            = 0.012_r8            ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kSiO3           = 0.0_r8             ! in marbl_parms framework, see NOTE above
         autotrophs(n)%Qp_fixed        =  Qp_zoo            ! only used for lvariable_PtoC=.false.
-        autotrophs(n)%gQfe_0          = 25.0e-6_r8
+        autotrophs(n)%gQfe_0          = 10.0e-6_r8
         autotrophs(n)%gQfe_min        = 3.0e-6_r8
-        autotrophs(n)%alphaPI_per_day = 0.32_r8
+        autotrophs(n)%alphaPI_per_day = 0.35_r8
         autotrophs(n)%PCref_per_day   = 5.0_r8
-        autotrophs(n)%thetaN_max      = 3.0_r8
+        autotrophs(n)%thetaN_max      = 3.2_r8
         autotrophs(n)%loss_thres      = 0.01_r8
         autotrophs(n)%loss_thres2     = 0.0_r8
         autotrophs(n)%temp_thres      = -10.0_r8
