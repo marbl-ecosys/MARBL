897d80d07003e557b1d19a93ad2eb09c796edfe6
kristenkrumhardt <kristen.krumhardt@colorado.edu>
2017-08-10 12:17:32 -0600

    decreased competitive ability of coccos, given new Tfunc

--- src/marbl_parms.F90
+++ src/marbl_parms.F90
@@ -288,7 +288,7 @@ contains
     do n=1,autotroph_cnt
       select case (trim(autotrophs_config(n)%sname))
         case ('sp')
-          autotrophs(n)%kFe             = 0.027e-3_r8         ! in marbl_parms framework, see NOTE above
+          autotrophs(n)%kFe             = 0.03e-3_r8         ! in marbl_parms framework, see NOTE above
           autotrophs(n)%kPO4            = 0.005_r8           ! in marbl_parms framework, see NOTE above
           autotrophs(n)%kDOP            = 0.3_r8             ! in marbl_parms framework, see NOTE above
           autotrophs(n)%kNO3            = 0.22_r8            ! in marbl_parms framework, see NOTE above
@@ -357,19 +357,19 @@ contains
           autotrophs(n)%loss_poc        = 0.0_r8
 
         case ('cocco')
-        autotrophs(n)%kFe             = 0.023e-3_r8         ! in marbl_parms framework, see NOTE above
+        autotrophs(n)%kFe             = 0.029e-3_r8         ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kPO4            = 0.006_r8           ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kDOP            = 0.3_r8             ! in marbl_parms framework, see NOTE above
-        autotrophs(n)%kNO3            = 0.25_r8            ! in marbl_parms framework, see NOTE above
+        autotrophs(n)%kNO3            = 0.3_r8            ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kNH4            = 0.012_r8            ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kSiO3           = 0.0_r8             ! in marbl_parms framework, see NOTE above
-        autotrophs(n)%kCO2            = 0.0_r8             !!!! added Km for CO2 for coccos
+        autotrophs(n)%kCO2            = 2.12_r8             !!!! added Km for CO2 for coccos
         autotrophs(n)%Qp_fixed        =  Qp_zoo            ! only used for lvariable_PtoC=.false.
         autotrophs(n)%gQfe_0          = 15.0e-6_r8
         autotrophs(n)%gQfe_min        = 3.0e-6_r8
-        autotrophs(n)%alphaPI_per_day = 0.33_r8
+        autotrophs(n)%alphaPI_per_day = 0.3_r8
         autotrophs(n)%PCref_per_day   = 5.0_r8
-        autotrophs(n)%thetaN_max      = 3.2_r8
+        autotrophs(n)%thetaN_max      = 2.8_r8
         autotrophs(n)%loss_thres      = 0.01_r8
         autotrophs(n)%loss_thres2     = 0.0_r8
         autotrophs(n)%temp_thres      = 0.0_r8
