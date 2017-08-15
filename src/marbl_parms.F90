82badc482fc09be392e9736af23857e17343f170
kristenkrumhardt <kristen.krumhardt@colorado.edu>
2017-08-15 09:55:32 -0600

    made light lim cocco same as for diat and really efficient Kfe for coccos

--- src/marbl_parms.F90
+++ src/marbl_parms.F90
@@ -357,7 +357,7 @@ contains
           autotrophs(n)%loss_poc        = 0.0_r8
 
         case ('cocco')
-        autotrophs(n)%kFe             = 0.026e-3_r8         ! in marbl_parms framework, see NOTE above
+        autotrophs(n)%kFe             = 0.015e-3_r8         ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kPO4            = 0.005_r8           ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kDOP            = 0.3_r8             ! in marbl_parms framework, see NOTE above
         autotrophs(n)%kNO3            = 0.25_r8            ! in marbl_parms framework, see NOTE above
@@ -367,9 +367,9 @@ contains
         autotrophs(n)%Qp_fixed        =  Qp_zoo            ! only used for lvariable_PtoC=.false.
         autotrophs(n)%gQfe_0          = 15.0e-6_r8
         autotrophs(n)%gQfe_min        = 3.0e-6_r8
-        autotrophs(n)%alphaPI_per_day = 0.33_r8
+        autotrophs(n)%alphaPI_per_day = 0.39_r8
         autotrophs(n)%PCref_per_day   = 5.0_r8
-        autotrophs(n)%thetaN_max      = 3.2_r8
+        autotrophs(n)%thetaN_max      = 4.0_r8
         autotrophs(n)%loss_thres      = 0.01_r8
         autotrophs(n)%loss_thres2     = 0.0_r8
         autotrophs(n)%temp_thres      = 0.0_r8
