915ed170b208e9f2b9d0e16239f70185a9399d52
Michael Levy <mike.levy.work@gmail.com>
2017-09-02 13:28:18 -0600

    Refactor str_to_array unit test
    pass str and expected_array to a subroutine, nothing else needs to change from
    test to test

--- tests/driver_src/marbl_utils_drv.F90
+++ tests/driver_src/marbl_utils_drv.F90
@@ -1,6 +1,8 @@
 module marbl_utils_drv
 
   use marbl_kinds_mod, only : r8
+  use marbl_kinds_mod, only : char_len
+  use marbl_logging,   only : marbl_log_type
 
   Implicit None
   Private
@@ -12,10 +14,7 @@ contains
 
   subroutine test(driver_status_log)
 
-    use marbl_logging,   only : marbl_log_type
     use marbl_utils_mod, only : marbl_utils_linear_root
-    use marbl_utils_mod, only : marbl_utils_str_to_array
-    use marbl_kinds_mod, only : char_len
 
     type(marbl_log_type),        intent(inout) :: driver_status_log
 
@@ -26,9 +25,6 @@ contains
     character(len=char_len)     :: str
     character(len=char_len), allocatable :: array(:), expected_array(:)
     integer                     :: test_cnt, pass_cnt, fail_cnt
-    logical                     :: ltest_fail
-    integer :: i
-
 
     ! Linear root tests
     test_cnt = 0
@@ -78,43 +74,11 @@ contains
     str = ".true."
     allocate(expected_array(1))
     expected_array(1) = ".true."
-    call marbl_utils_str_to_array(str, ",", array)
-    ltest_fail = (size(array) .ne. size(expected_array))
-    if (ltest_fail) then
+    if (str_to_array_test(str, expected_array, test_cnt, driver_status_log)) then
       fail_cnt = fail_cnt + 1
-      write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (str_to_array)"
-      call driver_status_log%log_noerror(log_message, subname)
-      write(log_message, "(A,I0,A)") "      expected ", &
-                                     size(expected_array), &
-                                     " element(s) in array"
-      call driver_status_log%log_noerror(log_message, subname)
-      write(log_message, "(A,I0,A)") "      result had ", &
-                                     size(array),         &
-                                     " element(s)"
-      call driver_status_log%log_noerror(log_message, subname)
     else
-      do i=1,size(array)
-        if (array(i) .ne. expected_array(i)) then
-          if (.not. ltest_fail) then
-            write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (str_to_array)"
-            call driver_status_log%log_noerror(log_message, subname)
-            ltest_fail = .true.
-          end if
-          write(log_message, "(2A)") "      Expected ", trim(expected_array(i))
-          call driver_status_log%log_noerror(log_message, subname)
-          write(log_message, "(2A)") "      Result   ", trim(array(i))
-          call driver_status_log%log_noerror(log_message, subname)
-        end if
-      end do
-      if (ltest_fail) then
-        fail_cnt = fail_cnt + 1
-      else
-        write(log_message, "(A,I0,A)") "PASS: Test ", test_cnt, " (str_to_array)"
-        call driver_status_log%log_noerror(log_message, subname)
-        pass_cnt = pass_cnt + 1
-      end if
+      pass_cnt = pass_cnt + 1
     end if
-    deallocate(array)
     deallocate(expected_array)
 
     ! (2) "123, 456" -> "123", " 456"
@@ -136,5 +100,57 @@ contains
 
 
   end function close_enough
+
+  function str_to_array_test(str, expected_array, test_cnt, driver_status_log) result(ltest_fail)
+
+    use marbl_utils_mod, only : marbl_utils_str_to_array
+
+    character(len=*),               intent(in)    :: str
+    character(len=*), dimension(:), intent(in)    :: expected_array
+    integer,                        intent(in)    :: test_cnt
+    type(marbl_log_type),           intent(inout) :: driver_status_log
+    logical :: ltest_fail
+
+    character(len=*), parameter :: subname = 'marbl_utils_drv:str_to_array_test'
+    character(len=char_len)     :: log_message
+
+    character(len=char_len), allocatable :: array(:)
+    integer :: i
+
+    call marbl_utils_str_to_array(str, ",", array)
+    ltest_fail = (size(array) .ne. size(expected_array))
+    if (ltest_fail) then
+      write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (str_to_array)"
+      call driver_status_log%log_noerror(log_message, subname)
+      write(log_message, "(A,I0,A)") "      expected ", &
+                                     size(expected_array), &
+                                     " element(s) in array"
+      call driver_status_log%log_noerror(log_message, subname)
+      write(log_message, "(A,I0,A)") "      result had ", &
+                                     size(array),         &
+                                     " element(s)"
+      call driver_status_log%log_noerror(log_message, subname)
+      deallocate(array)
+      return
+    end if
+    do i=1,size(array)
+      if (array(i) .ne. expected_array(i)) then
+        if (.not. ltest_fail) then
+          write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (str_to_array)"
+          call driver_status_log%log_noerror(log_message, subname)
+          ltest_fail = .true.
+        end if
+        write(log_message, "(2A)") "      Expected ", trim(expected_array(i))
+        call driver_status_log%log_noerror(log_message, subname)
+        write(log_message, "(2A)") "      Result   ", trim(array(i))
+        call driver_status_log%log_noerror(log_message, subname)
+      end if
+    end do
+    deallocate(array)
+    if (ltest_fail) return
+    write(log_message, "(A,I0,A)") "PASS: Test ", test_cnt, " (str_to_array)"
+    call driver_status_log%log_noerror(log_message, subname)
+
+  end function str_to_array_test
 
 end module marbl_utils_drv
