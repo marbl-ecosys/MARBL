7305c108680355a6a52903cc31eb8961d329f945
Michael Levy <mike.levy.work@gmail.com>
2017-09-02 11:13:06 -0600

    Move linear_root to marbl_utils_mod

--- src/marbl_diagnostics_mod.F90
+++ src/marbl_diagnostics_mod.F90
@@ -66,7 +66,6 @@ module marbl_diagnostics_mod
   private :: store_diagnostics_silicon_fluxes
   private :: store_diagnostics_iron_fluxes
   private :: compute_saturation_depth
-  private :: linear_root
 
   !-----------------------------------------------------------------------
   !  Largest possible size for each class of diagnostics
@@ -3557,6 +3556,8 @@ contains
 
   function compute_saturation_depth(marbl_domain, CO3, sat_val, marbl_status_log)
 
+    use marbl_utils_mod, only : marbl_utils_linear_root
+
     type(marbl_domain_type) , intent(in)    :: marbl_domain
     real(r8)                , intent(in)    :: CO3(:)
     real(r8)                , intent(in)    :: sat_val(:)
@@ -3591,9 +3592,9 @@ contains
        end do
 
        ! saturation depth is location of root of anomaly
-       compute_saturation_depth = linear_root(zt(k-1:k), anomaly(k-1:k), marbl_status_log)
+       compute_saturation_depth = marbl_utils_linear_root(zt(k-1:k), anomaly(k-1:k), marbl_status_log)
        if (marbl_status_log%labort_marbl) then
-         call marbl_status_log%log_error_trace('linear_root', subname)
+         call marbl_status_log%log_error_trace('marbl_utils_linear_root', subname)
          return
        end if
     end if
@@ -3601,39 +3602,6 @@ contains
     end associate
 
   end function compute_saturation_depth
-
-  !***********************************************************************
-
-  function linear_root(x,y, marbl_status_log)
-    ! TO-DO: if we end up with a marbl_math_mod, this can be generalized
-    !        to a better root-finding routine; otherwise maybe we compute
-    !        the root inside compute_saturation_depth rather than as a
-    !        separate function?
-
-    real(kind=r8), dimension(2), intent(in)    :: x,y
-    type(marbl_log_type),        intent(inout) :: marbl_status_log
-    real(kind=r8) :: linear_root
-
-    character(len=*), parameter :: subname = 'marbl_diagnostics_mod:linear_root'
-
-    real(kind=r8) :: m_inv
-
-    if ((y(1).gt.c0).and.(y(2).gt.c0)) then
-       call marbl_status_log%log_error("can not find root, both y-values are positive!", subname)
-       return
-    else if ((y(1).lt.c0).and.(y(2).lt.c0)) then
-       call marbl_status_log%log_error("can not find root, both y-values are negative!", subname)
-       return
-    end if
-
-    if (y(2).eq.c0) then
-       linear_root = x(2)
-    else
-       m_inv = (x(2)-x(1))/(y(2)-y(1))
-       linear_root = x(1)-m_inv*y(1)
-    end if
-
-  end function linear_root
 
   !***********************************************************************
 
--- src/marbl_utils_mod.F90
+++ src/marbl_utils_mod.F90
@@ -1,9 +1,47 @@
 module marbl_utils_mod
 
+  use marbl_kinds_mod, only : r8
+
+  use marbl_logging, only : marbl_log_type
+
   implicit none
   public
 
 contains
+
+  !***********************************************************************
+
+  function marbl_utils_linear_root(x,y, marbl_status_log) result(linear_root)
+    ! TO-DO: this can be generalized to a better root-finding routine
+
+    use marbl_constants_mod, only : c0
+
+    real(kind=r8), dimension(2), intent(in)    :: x,y
+    type(marbl_log_type),        intent(inout) :: marbl_status_log
+    real(kind=r8) :: linear_root
+
+    character(len=*), parameter :: subname = 'marbl_utils_mod:linear_root'
+
+    real(kind=r8) :: m_inv
+
+    if ((y(1).gt.c0).and.(y(2).gt.c0)) then
+       call marbl_status_log%log_error("can not find root, both y-values are positive!", subname)
+       return
+    else if ((y(1).lt.c0).and.(y(2).lt.c0)) then
+       call marbl_status_log%log_error("can not find root, both y-values are negative!", subname)
+       return
+    end if
+
+    if (y(2).eq.c0) then
+       linear_root = x(2)
+    else
+       m_inv = (x(2)-x(1))/(y(2)-y(1))
+       linear_root = x(1)-m_inv*y(1)
+    end if
+
+  end function marbl_utils_linear_root
+
+  !***********************************************************************
 
   subroutine marbl_utils_str_to_array(str, separator, array)
 
@@ -83,5 +121,7 @@ contains
     end do
 
   end subroutine marbl_utils_str_to_array
+
+  !***********************************************************************
 
 end module marbl_utils_mod
