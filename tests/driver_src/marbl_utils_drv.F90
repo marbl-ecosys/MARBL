3220c93acd0afcc23a96d59b17b613faa4d891ce
Michael Levy <mike.levy.work@gmail.com>
2017-09-02 13:17:41 -0600

    Add unit test for str_to_array()

--- tests/driver_src/marbl_utils_drv.F90
+++ tests/driver_src/marbl_utils_drv.F90
@@ -21,16 +21,20 @@ contains
 
     character(len=*), parameter :: subname = 'marbl_utils_drv:test'
     character(len=char_len)     :: log_message
+    real(kind=r8)               :: linear_root, expected_val, tol
     real(kind=r8), dimension(2) :: x, y
-    real(kind=r8) :: linear_root
-    real(kind=r8) :: expected_val
-    integer       :: test_cnt, pass_cnt, fail_cnt
+    character(len=char_len)     :: str
+    character(len=char_len), allocatable :: array(:), expected_array(:)
+    integer                     :: test_cnt, pass_cnt, fail_cnt
+    logical                     :: ltest_fail
+    integer :: i
 
 
     ! Linear root tests
     test_cnt = 0
     pass_cnt = 0
     fail_cnt = 0
+    tol = 1e-13_r8
 
     ! (1) root between (1,-1) and (2,1) is at x=2
     test_cnt = test_cnt + 1
@@ -42,19 +46,84 @@ contains
       call driver_status_log%log_error_trace('marbl_utils_linear_root', subname)
       return
     end if
-    if (close_enough(linear_root, expected_val, tol=1e-13_r8)) then
+    if (close_enough(linear_root, expected_val, tol)) then
       pass_cnt = pass_cnt + 1
       write(log_message, "(A,I0,A)") "PASS: Test ", test_cnt, " (linear root)"
     else
       fail_cnt = fail_cnt + 1
       write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (linear root)"
+      call driver_status_log%log_noerror(log_message, subname)
+      write(log_message, "(A, E24.16)") "       Expected ", expected_val
+      call driver_status_log%log_noerror(log_message, subname)
+      write(log_message, "(A, E24.16)") "       Result   ", linear_root
     end if
     call driver_status_log%log_noerror(log_message, subname)
 
     ! (2) root between (5,0) and (7,3) is at x=5
     ! (3) root between (5,3) and (7,0) is at x=7
 
+    if (fail_cnt .gt. 0) then
+      write(log_message, "(A,I0,A)") "Failed ", fail_cnt, " linear root test(s)"
+      call driver_status_log%log_error(log_message, subname)
+      driver_status_log%labort_marbl = .false.
+    end if
+
     ! str_to_array tests
+    test_cnt = 0
+    pass_cnt = 0
+    fail_cnt = 0
+
+    ! (1) ".true." -> ".true."
+    test_cnt = test_cnt + 1
+    str = ".true."
+    allocate(expected_array(1))
+    expected_array(1) = ".true."
+    call marbl_utils_str_to_array(str, ",", array)
+    ltest_fail = (size(array) .ne. size(expected_array))
+    if (ltest_fail) then
+      fail_cnt = fail_cnt + 1
+      write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (str_to_array)"
+      call driver_status_log%log_noerror(log_message, subname)
+      write(log_message, "(A,I0,A)") "      expected ", &
+                                     size(expected_array), &
+                                     " element(s) in array"
+      call driver_status_log%log_noerror(log_message, subname)
+      write(log_message, "(A,I0,A)") "      result had ", &
+                                     size(array),         &
+                                     " element(s)"
+      call driver_status_log%log_noerror(log_message, subname)
+    else
+      do i=1,size(array)
+        if (array(i) .ne. expected_array(i)) then
+          if (.not. ltest_fail) then
+            write(log_message, "(A,I0,A)") "FAIL: Test ", test_cnt, " (str_to_array)"
+            call driver_status_log%log_noerror(log_message, subname)
+            ltest_fail = .true.
+          end if
+          write(log_message, "(2A)") "      Expected ", trim(expected_array(i))
+          call driver_status_log%log_noerror(log_message, subname)
+          write(log_message, "(2A)") "      Result   ", trim(array(i))
+          call driver_status_log%log_noerror(log_message, subname)
+        end if
+      end do
+      if (ltest_fail) then
+        fail_cnt = fail_cnt + 1
+      else
+        write(log_message, "(A,I0,A)") "PASS: Test ", test_cnt, " (str_to_array)"
+        call driver_status_log%log_noerror(log_message, subname)
+        pass_cnt = pass_cnt + 1
+      end if
+    end if
+    deallocate(array)
+    deallocate(expected_array)
+
+    ! (2) "123, 456" -> "123", " 456"
+    ! (3) "'ABC, DEF', GHI" -> "'ABC, DEF', " GHI"
+
+    if (fail_cnt .gt. 0) then
+      write(log_message, "(A,I0,A)") "Failed ", fail_cnt, " str_to_array test(s)"
+      call driver_status_log%log_error(log_message, subname)
+    end if
 
   end subroutine test
 
