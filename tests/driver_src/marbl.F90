c887b4b7dd5bf86d5e3d5641b7e85380a04f2b02
Michael Levy <mike.levy.work@gmail.com>
2019-08-26 14:32:11 -0600

    Update bad comments following poor merge
    I resolved a couple of merge conflicts incorrectly in 3d83b20, refering to
    surface forcing instead of surface flux and interior forcing instead of
    interior tendency in a few comments and log messages.

--- tests/driver_src/marbl.F90
+++ tests/driver_src/marbl.F90
@@ -305,16 +305,16 @@ Program marbl
       call verify_single_instance(num_inst, trim(testname))
       lprint_marbl_log = .false.
       call marbl_init_test(marbl_instances(1), lshutdown = .false.)
-      ! Log surface forcing diagnostics passed back to driver
+      ! Log surface flux diagnostics passed back to driver
       associate(diags => marbl_instances(1)%surface_flux_diags%diags)
-        call driver_status_log%log_header('Surface forcing diagnostics', subname)
+        call driver_status_log%log_header('Surface flux diagnostics', subname)
         do n=1, size(diags)
           write(log_message, "(I0,7A)") n, '. ', trim(diags(n)%short_name), ': ', trim(diags(n)%long_name), &
                                         ' (units: ', trim(diags(n)%units),')'
           call driver_status_log%log_noerror(log_message, subname)
         end do
       end associate
-      ! Log interior forcing diagnostics passed back to driver
+      ! Log interior tendency diagnostics passed back to driver
       associate(diags => marbl_instances(1)%interior_tendency_diags%diags)
         call driver_status_log%log_header('Interior tendency diagnostics', subname)
         do n=1, size(diags)
