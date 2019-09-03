357055fbdd0c05164e98a305053f1805d1291714
Michael Levy <mike.levy.work@gmail.com>
2019-09-03 12:20:30 -0600

    disable useless-object-inheritance in pylintrc
    Rather than explicitly disabling the single instance of this pylint warning in
    code_consistency, we should disable all triggers of it so that moving forward
    the warning is not thrown.

--- MARBL_tools/code_consistency.py
+++ MARBL_tools/code_consistency.py
@@ -13,7 +13,7 @@ UNIBLOCK = u"\u2588"
 
 ##############
 
-class ConsistencyTestClass(object): # pylint: disable=useless-object-inheritance
+class ConsistencyTestClass(object):
     """
     This class contains all the Fortran consistency check tests.
     The logs dictionary contains a deque object with a list of all source lines
