de6cf813b551839c69d5584d30d14693f89702bb
Michael Levy <mike.levy.work@gmail.com>
2019-09-04 15:19:17 -0600

    Clean up netcdf_comparison.py script
    1. Public functions should be ds_comparison, not netcdf_comparison (i.e. open
       the files elsewhere and keep passing datasets around)
    2. Improve documentation
    3. dim names can be compared via listA == listB, no need to convert to sets and
       complicate things
    4. ds_compare_exact() calls ds_compare_loose(rtol=0, thres=0), and a single if
       statement in the latter makes sure the error message when two fields differ
       is unchanged.
    5. --strict loose should still check that values that are exactly 0 remain
       exactly 0

--- MARBL_tools/netcdf_comparison.py
+++ MARBL_tools/netcdf_comparison.py
@@ -29,59 +29,58 @@ DEFAULT_TOLS = {'rtol' : 1e-11, 'atol' : 1e-16, 'thres' : 1e-16}
 
 ##################
 
-def _init(baseline, new_file):
-    import xarray as xr
-
-    logger = logging.getLogger(__name__)
-    logger.info("Comparing %s to the baseline %s\n", new_file, baseline)
-    ds_base = xr.open_dataset(baseline)
-    ds_new = xr.open_dataset(new_file)
-    fail = False
-
-    # Compare file headers
-    header_fail, ds_base, ds_new = _header_test(ds_base, ds_new)
-    fail = fail or header_fail
-
-    return fail, ds_base, ds_new
-
-##################
-
-def netcdf_comparison_exact(baseline, new_file):
+def ds_comparison_exact(ds_base, ds_new):
     """
         Compare baseline to new_file using xarray
         Will only pass if two files are identical (bfb)
         (run with --strict=exact)
     """
-    header_fail, ds_base, ds_new = _init(baseline, new_file)
-
     # Compare remaining variables
-    return header_fail or _variable_check_exact(ds_base, ds_new)
+    return ds_comparison_loose(ds_base, ds_new, rtol=0, thres=0)
 
 ##################
 
-def netcdf_comparison_loose(baseline, new_file, rtol=DEFAULT_TOLS['rtol'],
-                            atol=DEFAULT_TOLS['atol'], thres=DEFAULT_TOLS['thres']):
+def ds_comparison_loose(ds_base, ds_new, rtol=DEFAULT_TOLS['rtol'], atol=DEFAULT_TOLS['atol'],
+                        thres=DEFAULT_TOLS['thres']):
     """
         Compare baseline to new_file using xarray
         Will pass if two files are within specified tolerance
         (run with --strict=loose)
     """
-    header_fail, ds_base, ds_new = _init(baseline, new_file)
+    header_fail, ds_base, ds_new = _reduce_to_matching_variables(ds_base, ds_new)
 
     # Compare remaining variables
     return header_fail or _variable_check_loose(ds_base, ds_new, rtol, atol, thres)
 
 ##################
 
-def _header_test(ds_base, ds_new):
+def _open_files(baseline, new_file):
     """
-        Are the file headers the same? Check for:
-            1. Variables in one file but not the other
+    Reads two netCDF files, returns xarray datasets
+    """
+    import xarray as xr
+
+    logger = logging.getLogger(__name__)
+    logger.info("Comparing %s to the baseline %s\n", new_file, baseline)
+
+    return xr.open_dataset(baseline), xr.open_dataset(new_file)
+
+##################
+
+def _reduce_to_matching_variables(ds_base, ds_new):
+    """
+        Are variables and dimensions the same in both datasets? Check for:
+            1. Variables in one dataset but not the other
             2. Variables that are different types
             3. Variables that are different dimensions
+               * check both name and size of dimension
+        Returns header_fail, ds_base, ds_new
+            * header_fail is True if any of three checks above fail
+            * ds_base and ds_new are same as inputs, except any variables that do not match the
+              above criteria are dropped from the dataset
     """
     logger = logging.getLogger(__name__)
-    fail = False
+    header_fail = False
     failed_vars = []
 
     # 1. Any variables in one file but not the other?
@@ -91,7 +90,7 @@ def _header_test(ds_base, ds_new):
     base_vars = list(set(base_vars) - common_vars)
     new_vars = list(set(new_vars) - common_vars)
     if base_vars:
-        fail = True
+        header_fail = True
         ds_base = ds_base.drop(base_vars)
         logger.info("The following variables are in the baseline file but not the new file:")
         base_vars.sort()
@@ -99,7 +98,7 @@ def _header_test(ds_base, ds_new):
             logger.info("* %s", var)
         logger.info("")
     if new_vars:
-        fail = True
+        header_fail = True
         ds_new = ds_new.drop(new_vars)
         logger.info("The following variables are in the new file but not the baseline file:")
         new_vars.sort()
@@ -120,8 +119,7 @@ def _header_test(ds_base, ds_new):
                 )
             )
         # (b) Do the dimension names match?
-        common_dims = set(ds_base[var].dims) & set(ds_new[var].dims)
-        if list(set(ds_base[var].dims) - common_dims) + list(set(ds_new[var].dims) - common_dims):
+        if ds_base[var].dims != ds_new[var].dims:
             err_messages.append(
                 "Baseline dimensions are {} and new file dimensions are {}".format(
                     ds_base[var].dims, ds_new[var].dims
@@ -137,52 +135,10 @@ def _header_test(ds_base, ds_new):
 
         # Report errors
         if _report_errs(var, err_messages):
-            fail = True
+            header_fail = True
             failed_vars.append(var)
 
-    ds_base = ds_base.drop(failed_vars)
-    ds_new = ds_new.drop(failed_vars)
-    return fail, ds_base, ds_new # return re-assigned ds_base and ds_new
-
-##################
-
-def _variable_check_exact(ds_base, ds_new):
-    """
-        Assumes both datasets contain the same variables with the same dimensions
-        Checks if two datasets are identical:
-        1. Are NaNs in the same place?
-        2. Are non-NaN values identical?
-    """
-
-    import numpy as np
-
-    var_check_fail = False
-    common_vars = list(set(ds_base.variables) & set(ds_new.variables))
-    common_vars.sort()
-
-    for var in common_vars:
-        err_messages = []
-        # (1) Are NaNs in the same place?
-        mask_base = ~np.isnan(ds_base[var].data)
-        mask_new = ~np.isnan(ds_new[var].data)
-        if np.any(mask_base ^ mask_new):
-            err_messages.append('NaNs are not in same place')
-
-        # (2) Compare non-NaN values
-        base_data = ds_base[var].data[mask_base]
-        new_data = ds_new[var].data[mask_base]
-        if np.any(base_data != new_data):
-            rel_err = np.where(base_data != 0, np.abs(new_data - base_data), 0) / \
-                      np.where(base_data != 0, np.abs(base_data), 1)
-            abs_err = np.abs(new_data - base_data)
-            err_messages.append("Values are not the same everywhere\n{}".format(
-                "    Max relative error: {}\n    Max absolute error: {}".format(np.max(rel_err),
-                                                                                np.max(abs_err))
-            ))
-
-        var_check_fail = _report_errs(var, err_messages) or var_check_fail
-
-    return var_check_fail
+    return header_fail, ds_base.drop(failed_vars), ds_new.drop(failed_vars)
 
 ##################
 
@@ -190,10 +146,13 @@ def _variable_check_loose(ds_base, ds_new, rtol, atol, thres):
     """
         Assumes both datasets contain the same variables with the same dimensions
         Checks:
-        1. If baseline is 0 at a given point, then ds_new must be 0 as well
-        2. Absolute vs relative error:
+        1. Are NaNs in the same place?
+        2. If baseline is 0 at a given point, then ds_new must be 0 as well
+        3. Absolute vs relative error:
            i. If 0 < |baseline value| <= thres then want absolute difference < atol
            ii. If |baseline value| > thres, then want relative difference < rtol
+           Note: if thres = 0 and rtol = 0, then this reduces to an exact test
+                 (Are non-NaN values identical?)
     """
     import numpy as np
 
@@ -205,39 +164,45 @@ def _variable_check_loose(ds_base, ds_new, rtol, atol, thres):
         err_messages = []
 
         # (1) Are NaNs in the same place?
-        if np.any(np.isnan(ds_base[var].data) ^ np.isnan(ds_new[var].data)):
+        mask = ~np.isnan(ds_base[var].data)
+        if np.any(mask ^ ~np.isnan(ds_new[var].data)):
             err_messages.append('NaNs are not in same place')
-        else:
-            mask = ~np.isnan(ds_base[var].data)
-            # (2) compare everywhere that baseline is 0 (well, <= thres)
-            if np.any(np.where(np.abs(ds_base[var].data[mask]) <= thres,
-                               np.abs(ds_new[var].data[mask]) > thres, False)):
-                err_messages.append('Baseline is 0 at some indices where new data is non-zero')
 
-            # (3) Compare everywhere that |baseline| is > thres
-            base_data = np.where(np.abs(ds_base[var].data[mask]) > thres,
-                                 ds_base[var].data[mask],
-                                 0)
-            new_data = np.where(np.abs(ds_base[var].data[mask]) > thres,
-                                ds_new[var].data[mask],
-                                0)
-            rel_err = np.where(base_data != 0, np.abs(new_data - base_data), 0) / \
-                      np.where(base_data != 0, np.abs(base_data), 1)
-            if np.any(rel_err > rtol):
-                err_messages.append("Max relative error ({}) exceeds {}".format(np.max(rel_err),
-                                                                                rtol))
+        # (2) compare everywhere that baseline is 0
+        if np.any(np.where(ds_base[var].data[mask] == 0, ds_new[var].data[mask] != 0, False)):
+            err_messages.append('Baseline is 0 at some indices where new data is non-zero')
 
-            # (4) Compare everywhere that 0 < |baseline| <= thres
-            base_data = np.where((ds_base[var].data[mask] != 0) &
-                                 (np.abs(ds_base[var].data[mask]) <= thres),
-                                 ds_base[var].data[mask], 0)
-            new_data = np.where((ds_base[var].data[mask] != 0) &
-                                (np.abs(ds_base[var].data[mask]) <= thres),
-                                ds_new[var].data[mask], 0)
-            abs_err = np.abs(new_data - base_data)
-            if np.any(abs_err > atol):
-                err_messages.append("Max absolute error ({}) exceeds {}".format(np.max(abs_err),
-                                                                                atol))
+        # (3i) Compare everywhere that 0 < |baseline| <= thres
+        base_data = np.where((ds_base[var].data[mask] != 0) &
+                             (np.abs(ds_base[var].data[mask]) <= thres),
+                             ds_base[var].data[mask], 0)
+        new_data = np.where((ds_base[var].data[mask] != 0) &
+                            (np.abs(ds_base[var].data[mask]) <= thres),
+                            ds_new[var].data[mask], 0)
+        abs_err = np.abs(new_data - base_data)
+        if np.any(abs_err > atol):
+            err_messages.append("Max absolute error ({}) exceeds {}".format(np.max(abs_err),
+                                                                            atol))
+
+        # (3ii) Compare everywhere that |baseline| is > thres
+        base_data = np.where(np.abs(ds_base[var].data[mask]) > thres,
+                             ds_base[var].data[mask],
+                             0)
+        new_data = np.where(np.abs(ds_base[var].data[mask]) > thres,
+                            ds_new[var].data[mask],
+                            0)
+        rel_err = np.where(base_data != 0, np.abs(new_data - base_data), 0) / \
+                  np.where(base_data != 0, np.abs(base_data), 1)
+        if np.any(rel_err > rtol):
+            if rtol == 0:
+                abs_err = np.abs(new_data - base_data)
+                err_messages.append("Values are not the same everywhere\n{}".format(
+                    "    Max relative error: {}\n    Max absolute error: {}".format(
+                        np.max(rel_err), np.max(abs_err))
+                ))
+            else:
+                err_messages.append("Max relative error ({}) exceeds {}".format(
+                    np.max(rel_err), rtol))
 
         var_check_fail = _report_errs(var, err_messages) or var_check_fail
 
@@ -257,6 +222,8 @@ def _report_errs(var, err_messages):
         logger.info("")
         return True
     return False
+
+##################
 
 def _parse_args():
     """ Parse command line arguments
@@ -293,6 +260,8 @@ def _parse_args():
 
     return parser.parse_args()
 
+##################
+
 if __name__ == "__main__":
     import os
 
@@ -302,13 +271,14 @@ if __name__ == "__main__":
     LOGGER = logging.getLogger(__name__)
 
     args = _parse_args() # pylint: disable=invalid-name
+    ds_base_in, ds_new_in = _open_files(args.baseline, args.new_file) # pylint: disable=invalid-name
     if args.strict == 'loose':
-        if netcdf_comparison_loose(args.baseline, args.new_file, args.rtol, args.atol, args.thres):
+        if ds_comparison_loose(ds_base_in, ds_new_in, args.rtol, args.atol, args.thres):
             LOGGER.error("Differences found between files!")
             os.sys.exit(1)
         LOGGER.info("PASS: All variables match and are within specified tolerance.")
     if args.strict == 'exact':
-        if netcdf_comparison_exact(args.baseline, args.new_file):
+        if ds_comparison_exact(ds_base_in, ds_new_in):
             LOGGER.error("Differences found between files!")
             os.sys.exit(1)
         LOGGER.info("PASS: All variables match and have exactly the same values.")
