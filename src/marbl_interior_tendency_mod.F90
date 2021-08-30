47def8cea575ce11466c12dd97efb957652a3f59
Michael Levy <mike.levy.work@gmail.com>
2021-08-30 13:06:38 -0600

    Fix bad array indexing
    Want to use all levels of POC%sed_flux(:) when computing other_remin(:), not
    just POC%sed_flux(k)

--- src/marbl_interior_tendency_mod.F90
+++ src/marbl_interior_tendency_mod.F90
@@ -3128,8 +3128,8 @@ contains
 
            flux_alt = POC%to_floor*1.0e-6_r8*spd*365.0_r8 ! convert to mmol/cm^2/year
            other_remin(1:k) = min(bot_flux_to_tend(1:k) * &
-                                  min(0.1_r8 + flux_alt, 0.5_r8) * (POC%to_floor - POC%sed_loss(k)), &
-                                      bot_flux_to_tend(1:k) * (POC%to_floor - POC%sed_loss(k)) - &
+                                  min(0.1_r8 + flux_alt, 0.5_r8) * (POC%to_floor - POC%sed_loss(1:k)), &
+                                      bot_flux_to_tend(1:k) * (POC%to_floor - POC%sed_loss(1:k)) - &
                                       (sed_denitrif(1:k)*denitrif_C_N))
 
            !----------------------------------------------------------------------------------
