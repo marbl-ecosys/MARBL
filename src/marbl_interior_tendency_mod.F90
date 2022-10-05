c8854113672ff6a67b5b82d9b03942ac0d4b7fbb
Michael Levy <mike.levy.work@gmail.com>
2022-10-05 14:06:54 -0600

    Add consistency check for PAR forcing
    Warn the GCM if the sum of the ice category fractions is not close to 1
    (threshold is hard-coded to be within 1e-6 of 1)

--- src/marbl_interior_tendency_mod.F90
+++ src/marbl_interior_tendency_mod.F90
@@ -230,7 +230,10 @@ contains
        return
     endif
 
-    associate(                                                      &
+    ! Verify forcing is consistent
+    call check_forcing(interior_tendency_forcings, interior_tendency_forcing_indices, marbl_status_log)
+
+   associate(                                                      &
          km                  => domain%km,                          &
          kmt                 => domain%kmt,                         &
          num_PAR_subcols     => domain%num_PAR_subcols,             &
@@ -3801,5 +3804,34 @@ contains
   end subroutine update_particulate_terms_from_prior_level
 
   !***********************************************************************
+
+  subroutine check_forcing(forcings, forcing_indices, marbl_status_log)
+
+    type(marbl_forcing_fields_type),                     intent(in)    :: forcings(:)
+    type(marbl_interior_tendency_forcing_indexing_type), intent(in)    :: forcing_indices
+    type(marbl_log_type),                                intent(inout) :: marbl_status_log
+
+    character(len=*), parameter :: subname = 'marbl_interior_tendency_mod:check_forcing'
+    character(len=char_len)     :: log_message
+    real(r8)                    :: col_frac_sum
+    integer                     :: m
+
+    ! If provided, total column fraction must sum to 1 (or close enough)
+    if (forcing_indices%PAR_col_frac_id > 0) then
+      col_frac_sum = sum(forcings(forcing_indices%PAR_col_frac_id)%field_1d(1,:))
+      if (abs(1._r8 - col_frac_sum) > 1e-6_r8) then
+        do m=1,size(forcings(forcing_indices%PAR_col_frac_id)%field_1d(:,:), 2)
+          write(log_message, "(A, I0, A, E11.3)") "PAR_col_frac(", m, ") = ", &
+                                                  forcings(forcing_indices%PAR_col_frac_id)%field_1d(1,m)
+          call marbl_status_log%log_warning(log_message, subname)
+        end do
+        write(log_message, "(A, E11.3, A)") "Total column fractions sum to ", col_frac_sum, &
+                                            ", which is not close to 1"
+        call marbl_status_log%log_warning(log_message, subname)
+        return
+      end if
+    end if
+
+  end subroutine check_forcing
 
 end module marbl_interior_tendency_mod
